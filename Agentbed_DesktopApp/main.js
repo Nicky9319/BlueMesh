a68baa0b1341ab6f4f3ada5d06ba38d969a4389d
Nicky9319 <paarthsaxena2005@gmail.com>
2025-06-09 19:41:57 +0530

    Electron JS Setup With Tailwind css

---
+++ Agentbed_DesktopApp/main.js
@@ -0,0 +1,78 @@
+// Imports and modules !!! ---------------------------------------------------------------------------------------------------
+
+import { app, BrowserWindow } from 'electron'
+import { join } from 'path'
+import { is } from '@electron-toolkit/utils'
+
+// Imports and modules END !!! ---------------------------------------------------------------------------------------------------
+
+
+
+
+// Variables and constants !!! ---------------------------------------------------------------------------------------------------
+
+let mainWindow;
+
+// Variables and constants END !!! ---------------------------------------------------------------------------------------------------
+
+
+
+
+
+// App Section !!! -------------------------------------------------------------------------------------
+
+function createWindow() {
+  // Create the browser window
+  mainWindow = new BrowserWindow({
+    width: 1440,
+    height: 1024,
+    show: false,
+    autoHideMenuBar: true,
+    webPreferences: {
+      preload: join(__dirname, '../preload/preload.js'),
+      sandbox: false,
+      contextIsolation: true,
+      devTools: true,
+    }
+  })
+
+  mainWindow.on('ready-to-show', () => {
+    mainWindow.show()
+  })
+
+  // Load the renderer process
+  if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
+    mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL'])
+  } else {
+    mainWindow.loadFile(join(__dirname, '../renderer/index.html'))
+  }
+
+  mainWindow.setMenuBarVisibility(false);
+}
+
+app.whenReady().then(() => {
+  // Single Instance Check 
+  const AppLock = app.requestSingleInstanceLock();
+  if (!AppLock) {
+    app.exit(0);
+  }
+
+  createWindow()
+
+  app.on('activate', function () {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+// Quit when all windows are closed, except on macOS
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+// App Section END !!! --------------------------------------------------------------------------------
